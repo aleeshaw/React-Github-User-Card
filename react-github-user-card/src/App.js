fd53c6c75db2e04eae852dbf540f5d5cda8dd095
aleesha-wood <spectrat@spectratart.com>
2019-11-01 23:20:55 -0400

    app taking in github user data, set to state. Usercard component created

--- react-github-user-card/src/App.js
+++ react-github-user-card/src/App.js
@@ -1,20 +1,32 @@
 import React from 'react';
 import logo from './logo.svg';
 import './styling/App.css';
+import axios from 'axios';
+import UserCard from './components/UserCard.js';
 
 class App extends React.Component {
-  state = {
-    user: []
-  };
+  state = {};
 
   componentDidMount() {
-    fetch("https://api.github.com/users/aleeshaw")
+    axios
+     .get("https://api.github.com/users/aleeshaw")
+     .then(res => {
+       console.log("results: ", res)
+       this.setState(
+        res.data
+      );
+       console.log(this.state);
+     })
+     .catch(error => {
+       console.log(error)
+     });
   }
 
   render() {
     return (
       <div className="App">
-        
+        <h1>GitHub User Card</h1>
+        <UserCard />
       </div>
     );
     }
--- react-github-user-card/src/components/UserCard.js
+++ react-github-user-card/src/components/UserCard.js
@@ -1,1 +1,9 @@
 import React from 'react';
+
+const UserCard = props => {
+  return (
+    <div>Card Stuff</div>
+  );
+};
+
+export default UserCard;

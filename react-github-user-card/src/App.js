72ebe0c22ed653499ab44c292f49666f3ab395d3
aleesha-wood <spectrat@spectratart.com>
2019-11-04 21:36:58 -0500

    testing user info get process

--- react-github-user-card/src/App.js
+++ react-github-user-card/src/App.js
@@ -7,7 +7,8 @@ import UserCard from './components/UserCard.js'
 class App extends React.Component {
   state = {
     user: {},
-    followers: []
+    followList: [],
+    followData: []
   }
 
   componentDidMount() {
@@ -23,47 +24,68 @@ class App extends React.Component {
      .catch(error => {
        console.log(error)
      });
+
     axios
      .get("https://api.github.com/users/aleeshaw/followers")
      .then(res => {
-       console.log("followers", res.data)
+       console.log(res.data)
        this.setState({
-         followers: res.data.map(user=> {
+         followList: res.data.map(user=> {
           return user.login
         })
        })
-       console.log('followers', this.state.followers)
+       console.log('follow list', this.state.followList)
+
+      //  this.state.followList.map(follower => {
+      //   axios
+      //    .get(`https://api.github.com/users/${follower}`)
+      //    .then(res => {
+      //      //console.log("follow data", res.data)
+      //      this.setState({
+      //        followData: res.data
+      //      })
+      //      console.log('data test', this.state.followData)
+      //    })
+      //    .catch(error => {
+      //      console.log(error)
+      //    });
+      //})
      })
      .catch(error => {
        console.log(error)
      });
+     
+     
   }
 
   render() {
     return (
       <div className="App">
         <h1>GitHub User Card</h1>
-        <UserCard 
-          name={this.state.user.name}
-          img={this.state.user.avatar_url}
-          bio={this.state.user.bio}
-          url={this.state.user.html_url}
-          repos={this.state.user.public_repos}
-          following={this.state.user.following}
-          followers={this.state.user.followers}
-        />
-        {/* {this.state.followers.map(follower => (
-           <UserCard 
-           key={follower.login.id}
-           name={follower.login.name}
-           img={follower.avatar_url}
-           bio={follower.bio}
-           url={follower.html_url}
-           repos={follower.public_repos}
-           following={follower.following}
-           followers={follower.followers}
-         />
-        ))} */}
+        <div className="usercards">
+          <UserCard 
+            name={this.state.user.name}
+            img={this.state.user.avatar_url}
+            bio={this.state.user.bio}
+            url={this.state.user.html_url}
+            repos={this.state.user.public_repos}
+            following={this.state.user.following}
+            followers={this.state.user.followers}
+          />
+          {this.state.followList.map(follower => {
+            axios.get(`https://api.github.com/users/${follower}`)
+            .then(res => {
+              console.log(res.data)
+              
+            })
+            .catch(error => {
+              console.log(error)
+            })
+            return (
+              <div>test</div>
+            )
+          })}
+        </div>
       </div>
     );
     }

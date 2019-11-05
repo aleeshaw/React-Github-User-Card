efe476a80f7e9e5a666e718b1bf7568857edff45
aleesha-wood <spectrat@spectratart.com>
2019-11-05 16:36:52 -0500

    usercard classnames added and usercard styled

--- react-github-user-card/src/components/UserCard.js
+++ react-github-user-card/src/components/UserCard.js
@@ -1,23 +1,26 @@
 import React from 'react';
+import '../styling/UserCard.css';
 
 const UserCard = props => {
   
   return (
     <div className="user-card">
       <div className="img-name-bio">
-      <img width="300" src={props.user.avatar_url} />
-      <a href={props.user.html_url}><h1>{props.user.name}</h1></a>
-      <h3>Bio: {props.user.bio}</h3>
+        <img width="300" src={props.user.avatar_url} />
+        <div className="name-bio">
+          <a href={props.user.html_url}><h1>{props.user.name}</h1></a>
+          <h3>Bio: {props.user.bio}</h3>
+        </div>
       </div>
       <div className="stats">
         <div className="stat">
-          Repos: <p>{props.user.public_repos}</p>
+          <p className="stat-title">Repos:</p> <p className="stat-num">{props.user.public_repos}</p>
         </div>
         <div className="stat">
-          Following: <p>{props.user.following}</p>
+          <p className="stat-title">Following:</p> <p className="stat-num">{props.user.following}</p>
         </div>
         <div className="stat">
-          Followers: <p>{props.user.followers}</p>
+          <p className="stat-title">Followers:</p> <p className="stat-num">{props.user.followers}</p>
         </div>
       </div>
     </div>
